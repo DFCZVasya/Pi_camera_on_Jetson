fa041a852dbee11dc518c76c653ba18bab554821
Василий Лопаткин <loopatkin.2002@outlook.com>
2020-01-27 16:52:38 +0700

    first commit

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def gstreamer_pipeline(
 
 
 def show_camera():
-	yolo = YOLO('model_data/yolo-tiny.h5', 'model_data/tiny_yolo_anchors.txt', 'model_data/coco_classes.txt')
+    yolo = YOLO('model_data/yolo-tiny.h5', 'model_data/tiny_yolo_anchors.txt', 'model_data/coco_classes.txt')
 
 
     cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
@@ -48,19 +48,19 @@ def show_camera():
             ret_val, frame = cap.read()
             frame = take_and_resize(frame)
             outBoxes = yolo.detect_image(frame)
-			frame = np.asarray(frame)
-			if len(outBoxes) > 0:
-				for box in outBoxes:
+            frame = np.asarray(frame)
+            if len(outBoxes) > 0:
+                for box in outBoxes:
 					# extract the bounding box coordinates
-					(x, y) = (int(box[0]), int(box[1]))
-					(w, h) = (int(box[2]), int(box[3]))
-					bbox = [x, y, w, h, box[4]]
-					output_classes.append(bbox[4])
-					cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0,255,0), 2)
-					text = 'classID = {}'.format(box[4])
-					cv2.putText(frame, text, (box[0], box[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
+                    (x, y) = (int(box[0]), int(box[1]))
+                    (w, h) = (int(box[2]), int(box[3]))
+                    bbox = [x, y, w, h, box[4]]
+                    output_classes.append(bbox[4])
+                    cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0,255,0), 2)
+                    text = 'classID = {}'.format(box[4])
+                    cv2.putText(frame, text, (box[0], box[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
 
-			cv2.imshow("CSI Camera", img)
+            cv2.imshow("CSI Camera", img)
             # This also acts as
             keyCode = cv2.waitKey(30) & 0xFF
             # Stop the program on the ESC key
